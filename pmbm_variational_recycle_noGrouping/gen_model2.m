dbc86076d889481ba5e99e4791fd2d22fc34354d
yuxuanx <xiayuxuan1993@outlook.com>
2017-01-02 10:48:00 +0100

    minor changes

--- pmbm_variational_recycle_noGrouping/gen_model2.m
+++ pmbm_variational_recycle_noGrouping/gen_model2.m
@@ -26,7 +26,7 @@ model.R= model.D*model.D';              %observation noise covariance
 model.Pd= 0.98;   %probability of detection in measurements
 
 % clutter parameters
-model.lfai= 10;                             %poisson average rate of uniform clutter (per scan)
+model.lfai= 30;                             %poisson average rate of uniform clutter (per scan)
 model.range_c= [ -100 100; -100 100 ];      %uniform clutter region
 model.lambda_fa= model.lfai/prod(model.range_c(:,2)-model.range_c(:,1)); %uniform clutter density
 
